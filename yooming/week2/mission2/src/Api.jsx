9126ec7a9d5154a182ab6d64b8b2a0e2567553cd
yoo_ming <128400178+ohyoom@users.noreply.github.com>
2024-10-06 17:03:45 +0900

    feat: 2주차 변경

--- yooming/week2/mission2/src/Api.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:3005/';
-
-export const fetchMovies = async () => {
-    const response = await axios.get(`${API_URL}/movies`);
-    return response.data;
-};
