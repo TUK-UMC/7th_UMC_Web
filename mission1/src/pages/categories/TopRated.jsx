1efebe35db31b1ae0e9f511a5ea483984cbdd384
minjeong9919 <minjeong9919@naver.com>
2024-10-30 15:11:35 +0900

    fix: 파일 위치 변경

--- mission1/src/pages/categories/TopRated.jsx
+++
@@ -1,6 +0,0 @@
-import { Layout } from "./Layout";
-import { getTopRatedMovies } from "../../apis/movieAPI";
-
-export const TopRated = () => {
-  return <Layout func={getTopRatedMovies} />;
-};
--- mission1/src/utils/formatImageURL.js
+++
@@ -1,3 +0,0 @@
-export const formatImageURL = (url) => {
-  return `https://image.tmdb.org/t/p/w200${url}`;
-};
