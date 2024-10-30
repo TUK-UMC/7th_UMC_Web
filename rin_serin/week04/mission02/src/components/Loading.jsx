881e9dd91f18673f020fbb42bd5ec80b65d849e6
minjeong9919 <minjeong9919@naver.com>
2024-10-30 14:39:32 +0900

    fix: 원격 저장소 연결 테스트 중

--- rin_serin/week04/mission02/src/components/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Loading = () => (
-    <div>
-        <h1 style={{ margin: '10px 10px' }}>영화를 불러오는 중입니다...</h1>
-    </div>
-);
-
-export default Loading;
--- 몽실_김민정/week2/mission1/src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import "./Button.css";
-
-export const Button = ({ children, ...rest }) => {
-  return (
-    <button className='button-container' {...rest}>
-      {children}
-    </button>
-  );
-};
