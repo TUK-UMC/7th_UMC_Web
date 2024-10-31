e7539ce60633495230447a71cd353c34eb5f6360
minjeong9919 <minjeong9919@naver.com>
2024-10-31 14:14:04 +0900

    feat: mission2 프로젝트 생성

---
+++ 몽실_김민정/week5/mission2/src/constants/errorMessage.js
@@ -0,0 +1,10 @@
+export const ERROR_MESSAGE = {
+  EMAIL: {
+    REQUIRED: "이메일을 입력해주세요.",
+    VALID: "유효한 이메일 주소를 입력해 주세요.",
+  },
+  PASSWORD: {
+    REQUIRED: "비밀번호를 입력해주세요.",
+    LENGTH: "비밀번호는 8자 이상, 16자리 이하여야 합니다.",
+  },
+};
