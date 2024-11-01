afdba2c65eb42eb4ae37c6a4d8d95cccff9a3ec5
minjeong9919 <minjeong9919@naver.com>
2024-11-01 11:44:54 +0900

    feat: 미션3 프로젝트 생성

---
+++ 몽실_김민정/week5/mission3/src/hooks/useFetch.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (func, ...args) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await func(args);
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
