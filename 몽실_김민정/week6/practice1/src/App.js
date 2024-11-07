03b09a45cc58e33d052a384c51eb6b2f113903a7
minjeong9919 <minjeong9919@naver.com>
2024-11-07 13:41:56 +0900

    feat: 실습1 완료

--- 몽실_김민정/week6/practice1/src/App.js
+++ 몽실_김민정/week6/practice1/src/App.js
@@ -1,51 +1,23 @@
-import { useState } from "react";
+import "./App.css";
+import { useContext } from "react";
 import { Button } from "./components/Button/Button";
 import { Input } from "./components/Input/Input";
-import "./App.css";
 import { ListItem } from "./components/ListItem/ListItem";
+import { TodoContext } from "./context/TodoContext";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, task: "투두 만들어보기" },
-    { id: 2, task: "공부 야무지게 하기" },
-  ]);
-
-  const [text, setText] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const addTodo = () => {
-    if (text !== "")
-      setTodos((prev) => [
-        ...prev,
-        { id: Math.floor(Math.random() * 100) + 2, task: text },
-      ]);
-    setText("");
-  };
-
-  const handleInputChange = (e) => {
-    setText(e.target.value);
-  };
-
-  const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((todo) => id !== todo.id));
-  };
-
-  const editTodo = (id, text) => {
-    setTodos((prev) =>
-      prev.map((item) =>
-        id === item.id && text !== "" ? { ...item, task: text } : item
-      )
-    );
-  };
+  const { text, setText, handleSubmit, addTodo, todos, deleteTodo, editTodo } =
+    useContext(TodoContext);
 
   return (
     <div className='container'>
       <h1 className='title'>할일을 등록하세요!</h1>
       <form onSubmit={handleSubmit} className='form' d>
-        <Input type='text' value={text} onChange={handleInputChange} />
+        <Input
+          type='text'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <Button onClick={addTodo} type='submit'>
           할 일 등록
         </Button>
@@ -53,6 +25,7 @@ function App() {
       <div className='list'>
         {todos.map((todo) => (
           <ListItem
+            key={todo.id}
             id={todo.id}
             task={todo.task}
             deleteTodo={deleteTodo}
