2fe27d77bf49a5ffd077fd1f91e09b63a7e35a27
minjeong9919 <minjeong9919@naver.com>
2024-11-17 13:29:56 +0900

    feat: react query tool 적용

--- 몽실_김민정/week7/mission1/src/App.js
+++ 몽실_김민정/week7/mission1/src/App.js
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { RootLayout } from "./layout/root-layout";
 import Home from "./pages//Home";
@@ -65,6 +67,13 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 export default App;
