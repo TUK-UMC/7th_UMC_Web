d15ca7d12184098a5e6b7c1ffcaf905e5d359841
minjeong9919 <minjeong9919@naver.com>
2024-12-12 13:21:21 +0900

    feat(7주차-미션3): pagination 버튼 구현

---
+++ 몽실_김민정/week7/mission3/src/components/PagenationButton.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+export const PagenationButton = ({
+  handleClickNextButton,
+  handleClickPreviousButton,
+  page,
+}) => {
+  return (
+    <Wrapper>
+      <Button onClick={handleClickPreviousButton} disabled={page === 1}>
+        이전
+      </Button>
+      {page} 페이지
+      <Button onClick={handleClickNextButton}>다음</Button>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+  background-color: ${({ theme, disabled }) =>
+    disabled ? theme.colors.white : theme.colors.primary};
+  border: none;
+  color: ${({ theme, disabled }) =>
+    disabled ? theme.colors.gray_100 : theme.colors.white};
+  border-radius: 5px;
+  padding: 5px 10px;
+  font-size: 16px;
+  font-weight: 600;
+  cursor: pointer;
+`;
---
+++ 몽실_김민정/week7/mission3/src/pages/Home.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { Poster } from "../components/Poster";
+import { getPopularMovies } from "../apis/movieAPI";
+import { PagenationButton } from "../components/PagenationButton";
+import { ErrorPage } from "./ErrorPage";
+import { useQuery } from "@tanstack/react-query";
+import { SkeletonPosterGrid } from "../components/SkeletonPosterGrid";
+
+function Home() {
+  const [page, setPage] = useState(1);
+
+  const handleClickNextButton = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  const handleClickPreviousButton = () => {
+    setPage((prev) => prev - 1);
+  };
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["homePageMovies", page],
+    queryFn: () => getPopularMovies(page),
+    keepPreviousData: true,
+  });
+
+  if (isLoading) {
+    return <SkeletonPosterGrid />;
+  }
+
+  if (error) {
+    return <ErrorPage />;
+  }
+
+  return (
+    <Container>
+      <PosterWrapper>
+        {data?.map((movie) => (
+          <Poster movieData={movie} key={movie.id} />
+        ))}
+      </PosterWrapper>
+      <PagenationButton
+        handleClickNextButton={handleClickNextButton}
+        handleClickPreviousButton={handleClickPreviousButton}
+        page={page}
+      />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PosterWrapper = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 10px;
+  padding: 15px 20px;
+`;
+
+export default Home;
